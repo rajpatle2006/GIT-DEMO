70e0967e5efed7777d4e5ee833e8add49b299e6f
RAJ PATLE <rajpatle799@gmail.com>
2025-12-03 16:14:02 +0530

    Codes

---
+++ Data type.java
@@ -0,0 +1,21 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 150000L;
+        float f = 5.75f;
+        double d = 19.99;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte value = " + b);
+        System.out.println("short value = " + s);
+        System.out.println("int value = " + i);
+        System.out.println("long value = " + l);
+        System.out.println("float value = " + f);
+        System.out.println("double value = " + d);
+        System.out.println("char value = " + c);
+        System.out.println("boolean value = " + bool);
+    }
+}
