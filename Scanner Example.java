0fdc6b4aa9da2434648b845d2fb4fb56bc582c65
RAJ PATLE <rajpatle799@gmail.com>
2025-12-04 18:09:01 +0530

    Add files via upload

---
+++ Scanner Example.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+  public static void main(String args[]){
+    Scanner scr = new Scanner(System.in);
+    int a = scr.nextInt();
+    System.out.print(a);
+  }
+}
